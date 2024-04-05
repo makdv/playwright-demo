8c37fa3ec0ebb186d8a9561820d267438d36142e
dmakarkin@alfabank.ru <dmakarkin@alfabank.ru>
2024-04-05 16:52:36 +0300

    fea: default project

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+    testIdAttribute: 'data-test-id',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [],
+});
